9faade9a8e627bc8be1031049429b1cd28e4ead2
Matt Fioccoprile <mfioccop@asu.edu>
2014-10-24 14:46:14 -0700

    Adding project files

---
+++ BeachTime/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeachTime.MvcApplication" Language="C#" %>
---
+++ BeachTime/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeachTime.Startup))]
+namespace BeachTime
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
