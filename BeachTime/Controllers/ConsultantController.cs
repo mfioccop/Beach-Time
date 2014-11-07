26b657aa9a1e0f36d38a7d20e3f9690b22f6e9d7
Matt Fioccoprile <mfioccop@asu.edu>
2014-11-07 13:37:43 -0700

    Started ConsultantController + related views

---
+++ BeachTime/Controllers/ConsultantController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeachTime.Controllers
+{
+	//[Authorize(Roles = "Consultant")]
+    public class ConsultantController : Controller
+    {
+        // GET: Consultant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Consultant/Edit/5
+        public ActionResult Edit(int id)
+        {
+			// Current Project
+			// Skill tags
+			// New email
+			// Confirm new email
+			// New password
+			// Confirm new password
+
+			// OLD PASSWORD???
+
+            return View();
+        }
+
+        // POST: Consultant/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+				// Current Project
+				// Skill tags
+				// New email
+				// Confirm new email
+				// New password
+				// Confirm new password
+
+				// OLD PASSWORD???
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+		// GET: Consultant/Upload
+	    public ActionResult Upload()
+	    {
+		    return PartialView("_Upload");
+	    }
+
+    }
+}
