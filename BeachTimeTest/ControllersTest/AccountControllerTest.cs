d349eecd5fa098902ad9339b16633f5f01646eda
bgarcia77 <b.garcia@asu.edu>
2014-11-06 17:10:00 -0700

    First tests
    Decided to push changes, so everyone could take a look before I go
    farther.

---
+++ BeachTimeTest/ControllersTest/AccountControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using BeachTime.Controllers;
+using BeachTime.Models;
+using System.Web.Mvc;
+
+namespace BeachTimeTest.ControllersTest
+{
+    [TestFixture]
+    class AccountControllerTest
+    {
+        [Test]
+        public void LoginActionReturnsLoginView()
+        {
+            string expected = "";
+            AccountController controller = new AccountController();
+
+            var result = controller.Login("/Account/Login") as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+    }
+}
