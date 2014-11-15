3dd49f7a544b873205d22ce4f9a2ce0985816069
Ian Hattendorf <ianhattendorf@gmail.com>
2014-11-14 18:07:22 -0700

    Add methods relating to beach status of users, closes #4

---
+++ BeachTime.Data/IUserBeachStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeachTime.Data {
+	using Microsoft.AspNet.Identity;
+
+	public interface IUserBeachStore {
+		bool OnBeach(BeachUser user);
+
+		IEnumerable<BeachUser> GetBeachedUsers();
+	}
+}
