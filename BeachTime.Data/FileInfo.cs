b05fbe0dfd079ab3508a39f98a9619be66bd3fe2
Ian Hattendorf <ianhattendorf@gmail.com>
2014-11-18 20:09:39 -0700

    Add FileInfo and IFileStore interface

---
+++ BeachTime.Data/FileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeachTime.Data {
+	public class FileInfo {
+		public int FileId { get; set; }
+		public int UserId { get; set; }
+		public string Path { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ BeachTime.Data/IFileStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeachTime.Data {
+	public interface IFileStore {
+
+		/// <summary>
+		/// Insert a new <see cref="FileInfo"/>
+		/// </summary>
+		/// <param name="fileInfo"/>
+		void Create(FileInfo fileInfo);
+
+		/// <summary>
+		/// Find a <see cref="FileInfo"/>
+		/// </summary>
+		/// <param name="fileId"/>
+		/// <returns/>
+		FileInfo FindByFileId(int fileId);
+
+		/// <summary>
+		/// Find all <see cref="FileInfo"/>s for a user
+		/// </summary>
+		/// <param name="userId"/>
+		/// <returns/>
+		IEnumerable<FileInfo> FindByUserId(int userId);
+
+		/// <summary>
+		/// Update a <see cref="FileInfo"/>
+		/// </summary>
+		/// <param name="fileInfo"/>
+		void Update(FileInfo fileInfo);
+
+		/// <summary>
+		/// Delete a <see cref="FileInfo"/>
+		/// </summary>
+		/// <param name="fileInfo"/>
+		void Delete(FileInfo fileInfo);
+	}
+}
