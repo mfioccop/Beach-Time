a6d32f78ec17954f3f1e9b1f10811d143bf33408
Matt Fioccoprile <mfioccop@asu.edu>
2014-11-20 14:54:09 -0700

    HomeNavbarViewModel contains relevant user data for the account dropdown plugin

--- BeachTime/Controllers/HomeController.cs
+++ BeachTime/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿namespace BeachTime.Controllers
+﻿using BeachTime.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace BeachTime.Controllers
 {
 	using System;
 	using System.Collections.Generic;
@@ -8,23 +12,69 @@
 
 	public class HomeController : Controller
 	{
+
+		#region UserManager_HelperMethods
+
+		/// <summary>
+		/// Manages user account registration and authentication
+		/// </summary>
+		private BeachUserManager _userManager;
+
+		/// <summary>
+		/// Gets the current UserManager
+		/// </summary>
+		public BeachUserManager UserManager
+		{
+			get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<BeachUserManager>(); }
+
+			private set { _userManager = value; }
+
+		}
+
+		private HomeNavbarViewModel getHomeNavbarViewModel()
+		{
+			var navbarViewModel = new HomeNavbarViewModel()
+			{
+				FirstName = String.Empty,
+				LastName = String.Empty,
+				Email = String.Empty,
+				Id = -1
+			};
+
+			// If no user is logged in, then return with the dummy user
+			if (User.Identity.GetUserId() == null) return navbarViewModel;
+			
+			// Otherwise find the user in the database and retrieve basic account information
+			var user = UserManager.FindById(User.Identity.GetUserId());
+
+			// Populate the view model with the proper info
+			navbarViewModel.FirstName = user.FirstName;
+			navbarViewModel.LastName = user.LastName;
+			navbarViewModel.Email = user.Email;
+			navbarViewModel.Id = user.UserId;
+
+			return navbarViewModel;
+		}
+
+		#endregion
+
 		public ActionResult Index()
 		{
-			return View();
+			return View(getHomeNavbarViewModel());
 		}
 
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
 
-			return View();
+			return View(getHomeNavbarViewModel());
 		}
 
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
 
-			return View();
+			return View(getHomeNavbarViewModel());
 		}
 	}
 }
---
+++ BeachTime/Models/HomeViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BeachTime.Models
+{
+	public class HomeNavbarViewModel
+	{
+		[Required]
+		public string FirstName { get; set; }
+		
+		[Required]
+		public string LastName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public int Id { get; set; }
+	}
+}
