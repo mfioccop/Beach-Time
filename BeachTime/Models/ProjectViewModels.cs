e72deb6d09dafce67961a5e822167c2895028767
Matt Fioccoprile <mfioccop@asu.edu>
2015-02-18 13:24:33 -0700

    Finished _Create/UpdateProject partial views, styled Executive index page

--- BeachTime/Models/ProjectViewModels.cs
+++ BeachTime/Models/ProjectViewModels.cs
@@ -10,19 +10,26 @@ namespace BeachTime.Models
 	public class ProjectCreateViewModel
 	{
 		[Required]
+		[Display(Name = "Project Name")]
 		public string ProjectName { get; set; }
+		
 		[Required]
+		[Display(Name = "Completed?")]
 		public bool IsCompleted { get; set; }
 
 	}
 	public class ProjectViewModel
 	{
 		[Required]
+		[Display(Name = "Project Name")]
 		public string ProjectName { get; set; }
+		
 		[Required]
+		[Display(Name = "Completed?")]
 		public bool IsCompleted { get; set; }
 
 		[Required]
+		[Display(Name = "Project ID")]		
 		public int ProjectId { get; set; }
 	}
 }
